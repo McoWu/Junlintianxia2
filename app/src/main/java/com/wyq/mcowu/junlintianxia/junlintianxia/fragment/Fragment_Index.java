691f490c0cab9efea4030f48c7c0cb0944d46619
tangxiaoying <2382771560@qq.com>
2017-12-25 21:06:02 +0800

    汤姐姐

--- app/src/main/java/com/wyq/mcowu/junlintianxia/junlintianxia/fragment/Fragment_Index.java
+++ app/src/main/java/com/wyq/mcowu/junlintianxia/junlintianxia/fragment/Fragment_Index.java
@@ -8,37 +8,46 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.stx.xhb.xbanner.XBanner;
+import com.stx.xhb.xbanner.transformers.Transformer;
 import com.wyq.mcowu.junlintianxia.R;
 import com.wyq.mcowu.junlintianxia.junlintianxia.adapter.SYAdapter1;
 import com.wyq.mcowu.junlintianxia.junlintianxia.bean.SYBean;
 import com.wyq.mcowu.junlintianxia.junlintianxia.presenter.SYPresentr;
-import com.wyq.mcowu.junlintianxia.junlintianxia.myapp.Imgloder;
 import com.wyq.mcowu.junlintianxia.junlintianxia.view.activity.MyView;
-import com.youth.banner.Banner;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.Unbinder;
 
 /**
  * Created by dell on 2017/12/25.
  */
 
-public class Fragment_Index extends Fragment implements MyView{
+public class Fragment_Index extends Fragment implements MyView,XBanner.XBannerAdapter{
 
-    Banner ban;
+   XBanner  xBanner;
+    Unbinder unbinder;
     RecyclerView recy1;
-    private List<String> list;
+    private List<String> imgesUrl;
+    private List<String> wenzi;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = View.inflate(getContext(), R.layout.fragment_index,null);
         //初始化控件
-        ban= (Banner) view.findViewById(R.id.ban);
+        xBanner= (XBanner) view.findViewById(R.id.ban);
+        xBanner.setmAdapter(this);
+        initBanner();
         recy1= (RecyclerView) view.findViewById(R.id.recy1);
+        //初始化XBanner轮播图
 
-        OkUrl();
+
         //首页 SYPresentr
         SYPresentr syPresentr=new SYPresentr(this);
         syPresentr.add();
@@ -46,20 +55,34 @@ public class Fragment_Index extends Fragment implements MyView{
 
         return view;
     }
-    //首页banner轮播图
-    private void OkUrl() {
-        list=new ArrayList<>();
-        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608f3b5Nc8d90151.jpg");
-        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608eb8cN9b9a0a39.jpg");
-        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608cae6Nbb1a39f9.jpg");
-        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608b7cdN218fb48f.jpg");
-        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/560b5a7eN214306c8.jpg");
-        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/560a409eN35e252de.jpg");
 
-        ban.isAutoPlay(true);
-        ban.setImageLoader(new Imgloder());
-        ban.setImages(list);
-        ban.start();
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+    //首页banner轮播图
+    private void  initBanner() {
+        //直接加图片的xbanner
+        wenzi = new ArrayList<>();
+        imgesUrl = new ArrayList<>();
+        wenzi.add("音箱狂欢");
+        wenzi.add("手机国庆礼");
+        wenzi.add("IT生活");
+        wenzi.add("母婴萌宝");
+        wenzi.add("国庆大礼包");
+        wenzi.add("手机大放假");
+
+        imgesUrl.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608f3b5Nc8d90151.jpg");
+        imgesUrl.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608eb8cN9b9a0a39.jpg");
+        imgesUrl.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608cae6Nbb1a39f9.jpg");
+        imgesUrl.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608b7cdN218fb48f.jpg");
+        imgesUrl.add("http://7mno4h.com2.z0.glb.qiniucdn.com/560b5a7eN214306c8.jpg");
+        imgesUrl.add("http://7mno4h.com2.z0.glb.qiniucdn.com/560a409eN35e252de.jpg");
+        xBanner.setData(imgesUrl, wenzi);
+        xBanner.setmAutoPalyTime(2000);
+        xBanner.setPageTransformer(Transformer.Rotate);    //立体旋转
+        xBanner.setPageChangeDuration(1500);
     }
     //首页热门活动数据
     @Override
@@ -73,6 +96,10 @@ public class Fragment_Index extends Fragment implements MyView{
         recy1.setAdapter(showAdapter);
     }
 
-
+    //xbanner立体旋转
+    @Override
+    public void loadBanner(XBanner banner, View view, int position) {
+        Glide.with(getActivity()).load(imgesUrl.get(position)).into((ImageView) view);
+    }
 }
 
