b36930edd325735eea79c7166512594f69d507ea
chengda1995 <747501646@qq.com>
2017-12-25 19:07:56 +0800

    Merge branch 'master' of github.com:McoWu/Junlintianxia2 into lcd
                     # Conflicts:
                     #	.idea/misc.xml
                     #	.idea/vcs.xml

--- app/src/main/java/com/wyq/mcowu/junlintianxia/junlintianxia/fragment/Fragment_Category.java
+++ app/src/main/java/com/wyq/mcowu/junlintianxia/junlintianxia/fragment/Fragment_Category.java
@@ -17,10 +17,12 @@ import com.wyq.mcowu.junlintianxia.junlintianxia.adapter.CategoryAdapter;
 import com.wyq.mcowu.junlintianxia.junlintianxia.base.BaseFragment;
 import com.wyq.mcowu.junlintianxia.junlintianxia.bean.Category;
 import com.wyq.mcowu.junlintianxia.junlintianxia.bean.Category_next;
+import com.wyq.mcowu.junlintianxia.junlintianxia.myapp.Imgloder;
 import com.wyq.mcowu.junlintianxia.junlintianxia.presenter.CategoryPresenter;
 import com.wyq.mcowu.junlintianxia.junlintianxia.view.activity.ICategory_View;
 import com.youth.banner.Banner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,7 +35,7 @@ public class Fragment_Category extends BaseFragment<CategoryPresenter> implement
     private RecyclerView mLeftRv;
     private Banner mBannerCate;
     private RecyclerView mRightRv;
-
+    private List<String> list;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -42,6 +44,7 @@ public class Fragment_Category extends BaseFragment<CategoryPresenter> implement
         initView(view);
         prsenter.show();
         prsenter.show(1);
+        initBanner();
         return view;
 
     }
@@ -56,6 +59,20 @@ public class Fragment_Category extends BaseFragment<CategoryPresenter> implement
     public void createPresenter() {
         prsenter = new CategoryPresenter(getContext(), this);
 
+    }
+    public void initBanner(){
+        list=new ArrayList<>();
+        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608f3b5Nc8d90151.jpg");
+        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608eb8cN9b9a0a39.jpg");
+        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608cae6Nbb1a39f9.jpg");
+        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/5608b7cdN218fb48f.jpg");
+        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/560b5a7eN214306c8.jpg");
+        list.add("http://7mno4h.com2.z0.glb.qiniucdn.com/560a409eN35e252de.jpg");
+
+        mBannerCate.isAutoPlay(true);
+        mBannerCate.setImageLoader(new Imgloder());
+        mBannerCate.setImages(list);
+        mBannerCate.start();
     }
 
     @Override
@@ -81,6 +98,12 @@ public class Fragment_Category extends BaseFragment<CategoryPresenter> implement
         mRightRv.setLayoutManager(manager);
         CateNextAdapter adapter = new CateNextAdapter(getActivity(),list);
         mRightRv.setAdapter(adapter);
+        adapter.setOnItemClickListener(new CateNextAdapter.onItemClickListener() {
+            @Override
+            public void ItemClick(View view, int position) {
+
+            }
+        });
     }
 
 }
