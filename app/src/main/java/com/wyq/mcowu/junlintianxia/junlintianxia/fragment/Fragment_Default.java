81eeb843ad230cc5c6a610a2394b95db86676fd5
chengda1995 <747501646@qq.com>
2017-12-27 20:08:29 +0800

    Merge branch 'master' of github.com:McoWu/Junlintianxia2 into lcd
                     # Conflicts:
                     #	.idea/misc.xml
                     #	.idea/vcs.xml

---
+++ app/src/main/java/com/wyq/mcowu/junlintianxia/junlintianxia/fragment/Fragment_Default.java
@@ -0,0 +1,10 @@
+package com.wyq.mcowu.junlintianxia.junlintianxia.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by dell on 2017/12/27.
+ */
+
+public class Fragment_default extends Fragment {
+}
