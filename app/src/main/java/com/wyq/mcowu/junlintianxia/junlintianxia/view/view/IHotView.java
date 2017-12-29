976b534066665d81e40dcf58b1ad92d736382829
chengda1995 <747501646@qq.com>
2017-12-29 20:57:08 +0800

    123

---
+++ app/src/main/java/com/wyq/mcowu/junlintianxia/junlintianxia/view/view/IHotView.java
@@ -0,0 +1,11 @@
+package com.wyq.mcowu.junlintianxia.junlintianxia.view.view;
+
+import com.wyq.mcowu.junlintianxia.junlintianxia.bean.HotBean;
+
+/**
+ * Created by dell on 2017/12/29.
+ */
+
+public interface IHotView {
+    void hot(HotBean hotBean);
+}
